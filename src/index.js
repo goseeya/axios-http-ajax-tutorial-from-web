cc1abf08e59b71a2b4d3c5a2b2470625bb37a3aa
goseeya <149542@gmail.com>
2019-07-21 09:54:37 +0200

    Axios instance

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
+//will be overwritten by axios instance
 axios.defaults.baseURL = 'http://jsonplaceholder.typicode.com';
 axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
